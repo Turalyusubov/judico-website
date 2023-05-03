e2d8243377ee72d9ebf3a166db8f2a25f58084b2
Turalyusubov <tu201906152@code.edu.az>
2023-05-03 23:31:38 +0400

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <>
+    <div className='font-open-sans relative'>
+      <Navbar />
       <Header />
 
-    </>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BiMenuAltRight } from 'react-icons/bi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+
+
     return (
-        <div>Navbar</div>
+        <nav className={`absolute w-full px-6 sm:px-12 lg:px-32 ${isOpen ? 'bg-stone-100 h-screen flex flex-col gap-10' : 'bg-transparent'}`}>
+            <div className=" flex w-full py-6 justify-between">
+                <div className="nav-logo text-gray-700 mr-10">
+                    <span className='font-martel font-bold text-2xl'>Judico</span>
+                </div>
+                <div className="nav-menu items-center hidden md:flex">
+                    <ul className='flex gap-8 text-gray-500'>
+                        <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                            <a href="#">Home</a>
+                        </li>
+                        <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                            <a href="#">Product</a>
+                        </li>
+                        <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                            <a href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <button onClick={() => setIsOpen(!isOpen)} className="nav-btn text-gray-500 text-4xl block md:hidden">
+                    {isOpen ? <AiOutlineClose /> : <BiMenuAltRight />}
+                </button>
+            </div>
+            <div className={`collapsed-menu flex-col items-center ${isOpen ? 'flex' : 'hidden'}`}>
+                <ul className='flex text-xl gap-8 text-gray-500 flex-col justify-center h-full'>
+                    <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                        <a href="#">Home</a>
+                    </li>
+                    <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                        <a href="#">Product</a>
+                    </li>
+                    <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                        <a href="#">Pricing</a>
+                    </li>
+                    <li className="nav-item transition duration-500 after:bg-gray-500 after:h-1 after:w-0 after:block hover:after:w-full">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
