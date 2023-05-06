55325b0d3dee1976476c69960a649e0a6a0ac519
Turalyusubov <tu201906152@code.edu.az>
2023-05-06 23:27:06 +0400

    created about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import About from './components/About'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <Features />
+      <About />
     </div>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import Image from '../assets/man-with-team.jpg'
+import People from '../assets/people.png'
+import Speed from '../assets/speed.png'
+import { BsFillPlayFill } from 'react-icons/bs'
 
 export default function About() {
     return (
-        <div>About</div>
+        <div className='flex flex-col justify-center bg-neutral-50 w-full px-6 sm:px-12 lg:px-32 py-20 flex flex-col items-center'>
+            <div className="sm:w-4/6 md:w-3/5 xl:w-2/5 text-center mb-20">
+                <h2 className='font-bold mb-4 text-gray-700 text-4xl font-martel'>Who We Are</h2>
+                <p className='text-gray-400 text-sm'>Problems trying to resolve the conflict between the two major realms
+                    of Classical physics: Newtonian mechanics  </p>
+            </div>
+            <div className="grid md:grid-cols-2 gap-20">
+                <div className="relative">
+                    <img className='w-full h-full object-cover' src={Image} alt="team" />
+                    <div className="absolute text-white z-10 bg-cyan-800 rounded-full text-3xl p-4 top-1/2 left-1/2">
+                        <BsFillPlayFill />
+                    </div>
+                    <div className="absolute top-0 bg-gradient-to-t from-neutral-800 to-transparent w-full h-full"></div>
+                </div>
+                <div className="flex flex-col justify-center">
+                    <h3 className='font-bold mb-4 text-gray-700 text-4xl font-martel'>Most trusted in our field</h3>
+                    <p className='text-gray-400 text-sm mb-10'>Most calendars are designed for teams. Slate
+                        is designed for freelancers who want a
+                        simple way to plan their schedule.</p>
+                    <div className="flex items-start gap-6 mb-8">
+                        <img className='object-contain' src={People} alt="people" />
+                        <div className=" flex flex-col gap-2">
+                            <p className='font-martel font-bold text-gray-700'>the quick fox jumps over the lazy dog</p>
+                            <p className='text-gray-500 font-bold text-sm'>Things on a very small scale ...</p>
+                        </div>
+                    </div>
+                    <div className="flex items-start gap-6">
+                        <img className='object-contain' src={People} alt="people" />
+                        <div className=" flex flex-col gap-2">
+                            <p className='font-martel font-bold text-gray-700'>the quick fox jumps over the lazy dog</p>
+                            <p className='text-gray-500 font-bold text-sm'>Things on a very small scale ...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
