ebf1e2d0b57263df021967cba84594b5f8bca7ed
Turalyusubov <tu201906152@code.edu.az>
2023-05-07 19:36:45 +0400

    created team section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Features from './components/Features'
 import About from './components/About'
 import Testimonials from './components/Testimonials'
+import Team from './components/Team'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Features />
       <About />
       <Testimonials />
+      <Team />
     </div>
   )
 }
