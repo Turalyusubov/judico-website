86a181879ee491d13c25928e483c2e19beaf394c
Turalyusubov <tu201906152@code.edu.az>
2023-05-07 19:13:57 +0400

    created testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
 import About from './components/About'
+import Testimonials from './components/Testimonials'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Features />
       <About />
+      <Testimonials />
     </div>
   )
 }
