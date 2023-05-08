e6ec4ba08a902f561bd0d28766fa2523294f1182
Turalyusubov <tu201906152@code.edu.az>
2023-05-08 21:16:43 +0400

    created footer section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/About'
 import Testimonials from './components/Testimonials'
 import Team from './components/Team'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Testimonials />
       <Team />
       <Contact />
+      <Footer />
     </div>
   )
 }
